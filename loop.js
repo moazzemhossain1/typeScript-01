744cbe05e9f627fb274177952b45123b0fccecd7
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-15 06:42:26 +0800

    use in typeScript loop

--- loop.js
+++ loop.js
@@ -1,17 +1,4 @@
 "use strict";
-let a = 10;
-let b = 20;
-if (a > b) {
-    console.log(a);
-}
-else {
-    console.log(b);
-}
-let name1 = 'Rogime';
-let name2 = "Karimei";
-if (name1.length > name2.length) {
-    console.log(name1);
-}
-else {
-    console.log(name2);
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
 }
---
+++ loop.ts
@@ -0,0 +1,13 @@
+
+
+for(let i:number=0;i<=10;i++){
+    console.log(i)
+}
+
+
+
+let i:number=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
