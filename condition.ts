24f25c1ceba9605dcc5db73cc9b84041e71b4035
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-15 06:35:24 +0800

    use typeScript condition

---
+++ condition.ts
@@ -0,0 +1,21 @@
+let a:number=10;
+let b:number=20;
+
+if(a>b){
+   console.log(a)
+}
+else{
+    console.log(b)
+}
+
+
+
+let name1:string='Rogime';
+let name2:string="Karimei";
+
+if(name1.length>name2.length){
+    console.log(name1)
+}
+else{
+    console.log(name2)
+}
---
+++ loop.js
@@ -0,0 +1,17 @@
+"use strict";
+let a = 10;
+let b = 20;
+if (a > b) {
+    console.log(a);
+}
+else {
+    console.log(b);
+}
+let name1 = 'Rogime';
+let name2 = "Karimei";
+if (name1.length > name2.length) {
+    console.log(name1);
+}
+else {
+    console.log(name2);
+}
