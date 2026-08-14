5257e246ac56676231b96525cde3f08f1a97dbb1
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-15 07:20:18 +0800

    use destring

---
+++ distructiong.js
@@ -0,0 +1,9 @@
+"use strict";
+let usersr = {
+    namee: "Moazzem Hossain",
+    roll: 101,
+    id: 1
+};
+const { namee, roll, id } = usersr;
+console.log(namee);
+console.log(roll);
---
+++ distructiong.ts
@@ -0,0 +1,21 @@
+
+
+let usersr={
+    namee: "Moazzem Hossain",
+    roll: 101,
+    id: 1
+}
+
+const {namee,roll,id}=usersr;
+console.log(namee);
+console.log(roll)
+
+
+let frinds=[
+    "Rahim",
+    "Karim",
+    "Sakib",
+    "Nafis",
+    "Hasan"
+]
+const []=[frinds]
