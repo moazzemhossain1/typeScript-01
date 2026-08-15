a4869b314d721e8fc18bc527694f996b63268079
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-15 16:48:39 +0800

    problem-04

---
+++ problem-04.js
@@ -0,0 +1,9 @@
+"use strict";
+let user = ["Karime", 22];
+console.log(user);
+let students = [
+    "Moazzem",
+    333,
+    true
+];
+console.log(students);
---
+++ problem-04.ts
@@ -0,0 +1,13 @@
+
+
+let user:[string,number]=["Karime",22];
+console.log(user)
+
+
+
+let students:[string,number,boolean]=[
+    "Moazzem",
+    333,
+    true
+]
+console.log(students)
