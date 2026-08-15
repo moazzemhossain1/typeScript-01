3f81eaa6abf9fc1300977a8e634f9ce25f20a027
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-15 18:13:51 +0800

    problem -09

---
+++ problem-09.js
@@ -0,0 +1,4 @@
+"use strict";
+let a = 10;
+let retlust = a % 2 === 0 ? "even" : "odd";
+console.log(retlust);
---
+++ problem-09.ts
@@ -0,0 +1,5 @@
+
+
+let a:number=10;
+let retlust=a%2===0?"even":"odd";
+console.log(retlust)
