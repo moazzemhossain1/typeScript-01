0b6fd8a8c7d9bfdb5f80c4d2a8bfee09adf9c542
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-15 18:09:57 +0800

    problem -08

---
+++ problem-08.js
@@ -0,0 +1,5 @@
+"use strict";
+let number1 = [1, 2, 3, 4, 5];
+let number2 = [6, 7, 8, 9];
+let marjeArray = [...number1, ...number2];
+console.log(marjeArray);
---
+++ problem-08.ts
@@ -0,0 +1,6 @@
+
+
+let number1:number[]=[1,2,3,4,5];
+let number2:number[]=[6,7,8,9];
+let marjeArray:number[]=[...number1,...number2]
+console.log(marjeArray)
